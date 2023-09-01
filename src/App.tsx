2719133bb2f5dab549e094aa4e0ab2758c9e02f4
yesoryeseul <yesoryeseul07@gmail.com>
2023-09-01 22:23:32 +0900

    feat: emotion 라이브러리 의존성 추가 및 글로벌 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Global } from "@emotion/react";
+import { globalStyles } from "./styles/global";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Global styles={globalStyles} />
+      <h1>test</h1>
+    </>
   );
 }
 
---
+++ src/styles/global.tsx
@@ -0,0 +1,14 @@
+import { css } from "@emotion/react";
+
+export const globalStyles = css`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  ul,
+  li {
+    list-style: none;
+  }
+`;
